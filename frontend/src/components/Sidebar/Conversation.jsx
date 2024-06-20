5ceab60876a3b2a386e780632c5259eedfc862e2
Bhagyashree Ganesh Tekade <bhagyashreetekade609@gmail.com>
2024-06-20 19:28:59 +0530

    Users added at sidebar & zustand is created for conversation selection

--- frontend/src/components/Sidebar/Conversation.jsx
+++ frontend/src/components/Sidebar/Conversation.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
+import React from 'react';
+import useConversation from '../../zustand/useConversation';
 
-const Conversation = () => {
+const Conversation = ({conversation,lastIndx,emoji}) => {
+    const {selectedConversation, setSelectedConversation} = useConversation();
+
+    const isSelected = selectedConversation?._id === conversation._id;
+
   return (<>
-        <div className=' flex gap-2 items-center hover:bg-orange-500 rounded p-2 py-1 cursor-pointer'>
+        <div className={`flex gap-2 items-center hover:bg-orange-500 rounded p-2 py-1 cursor-pointer ${isSelected ? "bg-orange-500" : ""}`}
+        onClick={()=> setSelectedConversation(conversation)}>
+            
             <div className='avatar online'>
                 <div className='w-12 rounded-full'>
-                    <img src="" alt="user avatar" />
+                    <img src={conversation.profilePicture} alt="user avatar" />
                 </div>
             </div>
 
             <div className='flex flex-col flex-1'>
                 <div className='flex gap-3 justify-between'>
-                    <p className='font-bold text-gray-200'>Ram</p>
-                    <span className='text-xl'>🎃</span>
+                    <p className='font-bold text-gray-200'>{conversation.fullName}</p>
+                    <span className='text-xl'>{emoji}</span>
                 </div>
             </div>
         </div>
-
-        <div className='divider my-0 py-0 h-1'/>
+        {/* divider */}
+        {!lastIndx && <div className='divider my-0 py-0 h-1'/> }
     </>
   )
 }
--- frontend/src/components/Sidebar/Conversations.jsx
+++ frontend/src/components/Sidebar/Conversations.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import Conversation from './Conversation'
+import useGetConversations from '../../hooks/useGetConversations';
+import { getRandomEmoji } from '../../utils/emojis';
 
 const Conversations = () => {
+  const {loading,conversations} = useGetConversations();
+  console.log(conversations)
+  
   return (
     <div className='py-2 flex flex-col overflow-auto'>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
+
+      { (conversations?.users?.map((conversation,idx) => (
+        <Conversation key={conversation._id}
+        conversation={conversation}
+        emoji = {getRandomEmoji()}
+        //this last index is maintain because we do not have to add the divider at the last conversation
+        lastIndx = {idx === conversations?.users?.length - 1}
+        />
+      )))}
+
+      {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
     </div>
   )
 }
