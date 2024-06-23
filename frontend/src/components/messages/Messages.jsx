8be4530b020b18ad296e4775c6ba5afe522db90b
Bhagyashree Ganesh Tekade <bhagyashreetekade609@gmail.com>
2024-06-23 18:44:00 +0530

    search functionality added

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Message from "./Message";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 
 const Messages = () => {
   const { loading, messages } = useGetMessages();
-  console.log(messages);
+  const lastMessageRef = useRef();
+  useEffect(()=>{
+    setTimeout(()=>{
+      lastMessageRef.current?.scrollIntoView({behavoiur:"smooth"});
+    },100);
+  },[messages])
 
   return (
     <div className="px-4 flex-1 overflow-auto">
@@ -13,7 +18,9 @@ const Messages = () => {
         messages &&
         messages.length > 0 &&
         messages?.map((message) => (
-          <Message key={message._id} message={message} />
+          <div key={message._id} ref={lastMessageRef}>
+            <Message message={message} />
+          </div>
         ))}
       {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
